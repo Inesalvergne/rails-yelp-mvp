63b0ac32c18a8ba09dddf6658efb2bc3a00b1853
Ines Alvergne <i.alvergne@gmail.com>
2022-04-12 20:23:30 -0400

    add new and create actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,23 +8,25 @@ class RestaurantsController < ApplicationController
   def show; end
 
   def new
+   @restaurant = Restaurant.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
