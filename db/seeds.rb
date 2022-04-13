438547ba63e86f9acf0820442254284584589b72
Ines Alvergne <i.alvergne@gmail.com>
2022-04-12 20:15:47 -0400

    adding index and show actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create!({ name: 'La banquise', address: '3 rue Rachel', category: 'french' })
+Restaurant.create!({ name: 'Yokato Yokabai Ramen', address: '10 rue Rachel', category: 'japanese' })
+Restaurant.create!({ name: 'Roma', address: '40 rue parc', category: 'italian' })
+Restaurant.create!({ name: 'Leméac', address: '100 avenue Laurier', category: 'french' })
+Restaurant.create!({ name: 'Plus de frites', address: '50 rue Michel', category: 'belgian' })
