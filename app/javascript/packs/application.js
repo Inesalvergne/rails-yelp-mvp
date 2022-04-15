2dec61a222d4ee1933116547157302f6a56d4b09
Ines Alvergne <i.alvergne@gmail.com>
2022-04-14 21:45:19 -0400

    add review form on the restaurant show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,16 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "channels"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
+console.log("coucou")
+
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
